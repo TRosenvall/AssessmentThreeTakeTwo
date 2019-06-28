2e934f05a365a5b4b22bd736d94dc97e67c7868a
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-06-28 14:54:35 -0600

    Search Bar

--- AssessmentThreeTakeTwo/Controllers/View Controllers/MovieListTableViewController.swift
+++ AssessmentThreeTakeTwo/Controllers/View Controllers/MovieListTableViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class MovieListTableViewController: UITableViewController {
 
+    @IBOutlet weak var searchBarText: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
@@ -31,50 +34,19 @@ class MovieListTableViewController: UITableViewController {
         }
         return cell
     }
+}
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+extension MovieListTableViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text, searchTerm != "" else {return}
+        
+        MovieController.sharedInstance.fetchSearchResults(searchText: searchTerm, completion: { (results) in
+            if let unwrappedSearchResults = results {
+                MovieController.sharedInstance.movies = unwrappedSearchResults
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        })
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
