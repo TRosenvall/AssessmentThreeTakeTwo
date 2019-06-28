2f8e26334669d386fc9cdcae2a523ac2e8f3533b
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-06-28 14:42:26 -0600

    Storyboard Setup

---
+++ AssessmentThreeTakeTwo/Controllers/View Controllers/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  AssessmentThreeTakeTwo
+//
+//  Created by Timothy Rosenvall on 6/28/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
